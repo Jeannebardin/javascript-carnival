f77776069ecaac73601345f1493a0a166ec9d078
jeannebardin <jmbardin@gmail.com>
2022-05-15 17:40:53 +1000

    dress the clown initial commit

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,71 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+document.onkeydown = checkKey
+
+function checkKey(e) {
+  console.log('i pushed up')
+
+  if (e.keycode == '38') {
+    changeVertical(-1)
+  } else if (e.keyCode == '40') {
+    changeVertical(1)
+  } else if (e.keyCode == '37') {
+    changeHorizontal(-1)
+    // left arrow
+  } else if (e.keyCode == '39') {
+    changeHorizontal(1)
+    //right arrow
+  }
+}
+
+// console.log("Dress The Clown!")
+var headIndex = 0
+var bodyIndex = 0
+var shoesIndex = 0
+
+var mainIndex = 0
+
+var head = document.getElementById('head')
+var body = document.getElementById('body')
+var shoes = document.getElementById('shoes')
+
+function changeHorizontal(shift) {
+  if (mainIndex == 0) {
+    headIndex += shift
+
+    if (headIndex < 0) headIndex = 5
+
+    if (headIndex > 5) headIndex = 0
+
+    head.src = './images/head' + headIndex + '.png'
+  }
+}
+
+if (mainIndex == 1) {
+  bodyIndex += shift
+
+  if (bodyIndex < 0) bodyIndex = 5
+
+  if (bodyIndex > 5) bodyIndex = 0
+
+  body.src = './images/body' + bodyIndex + '.png'
+}
+
+if (mainIndex == 2) {
+  shoesIndex += shift
+
+  if (shoesIndex < 0) shoesIndex = 5
+
+  if (shoesIndex > 5) shoesIndex = 0
+
+  shoes.src = './images/shoes' + shoesIndex + '.png'
+}
+
+function changeVertical(shift) {
+  mainIndex += shift
+
+  if (mainIndex < 0) mainIndex = 2
+  if (mainIndex > 2) mainIndex = 0
+}
