102f121afefd32fe8dca9e1a7c4d58dab0d8dae6
jeannebardin <jmbardin@gmail.com>
2022-05-15 12:41:24 +1000

    initial commit

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,17 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+var uni0 = document.getElementById('uni0')
+var uni1 = document.getElementById('uni1')
+var uni2 = document.getElementById('uni2')
+
+uni0.onclick = clicked
+uni1.onclick = clicked
+uni2.onclick = clicked
+
+function clicked() {
+  console.log('I am a unicorn')
+}
+
+console.log('Inflate The Unicorn!')
