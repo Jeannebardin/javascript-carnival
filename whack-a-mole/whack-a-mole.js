0c0e553000ee24d38e0c2554653ff2fbd9be96ec
jeannebardin <jmbardin@gmail.com>
2022-05-15 19:43:32 +1000

    initial commit

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,12 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+var cells = document.getElementsByTagName('TD')
+
+var randomIndex = Math.floor(Math.random() * cells.length)
+var randomCell = cells[randomIndex]
+
+console.log(randomCell)
