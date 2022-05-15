9d66fec9db56f1e145eec46e5fbe6725fb560824
jeannebardin <jmbardin@gmail.com>
2022-05-15 19:59:35 +1000

    randomised mole

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -7,6 +7,10 @@ console.log('Whack-a-Mole!')
 var cells = document.getElementsByTagName('TD')
 
 var randomIndex = Math.floor(Math.random() * cells.length)
+
 var randomCell = cells[randomIndex]
 
-console.log(randomCell)
+var mole = document.createElement('img')
+mole.src = './mole.PNG'
+mole.id = 'mole'
+randomCell.appendChild(mole)
