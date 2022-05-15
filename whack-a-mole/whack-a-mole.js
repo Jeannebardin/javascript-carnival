359ccac00ccca46af32f6528bfbbae1fa1fb363e
jeannebardin <jmbardin@gmail.com>
2022-05-15 20:12:52 +1000

    whack a mole broken

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -14,3 +14,14 @@ var mole = document.createElement('img')
 mole.src = './mole.PNG'
 mole.id = 'mole'
 randomCell.appendChild(mole)
+
+mole.onclock = whackedMole
+
+function whackedMole() {
+  randomIndex = Math.floor(Math.random() * cells.length)
+  randomCell = cells[randomIndex]
+  randomCell.appendChild(mole)
+
+  var audio = new Audio('./whack-audio.wav')
+  audio.play()
+}
